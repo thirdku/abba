cacf5e3b4b8b354feaac31518aa7aefea3bfedfd
thirdku <49618648+thirdku@users.noreply.github.com>
2020-03-03 14:34:16 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,29 +4,51 @@ export default class App extends React.Component{
     constructor(props){
         super(props);
         this.state ={
-            inputValue : '' , text :[]};
+         currentItem:{ 
+             inputValue : '',
+             key: ''
+            }, 
+             text :[]};
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
     }
      handleChange = (event) =>{
-     this.setState({ inputValue: event.target.value});
+     this.setState({ 
+        currentItem:{ 
+    inputValue:event.target.value,
+    key:Date.now()}
+        });
      }
      handleSubmit = (event) => {
         event.preventDefault();
+        const newItem = this.state.currentItem;
         this.setState({
-            inputValue :'',
-            text :[...this.state.text, this.state.inputValue]
+            text :[...this.state.text, newItem],
+            currentItem:{ 
+                inputValue : '',
+                key: ''
+               },
         });
+          
+     }
+     deleteItem(key){
+         const filteredItems = this.state.text.filter(item =>
+            item.id!==key);
+            this.setState({
+                text:filteredItems
+            });
      }
       
     render(){
         return (<div>
         <h1>To Do List</h1>
-             <ToDoList text={this.state.text}/>
+             <ToDoList text={this.state.text} 
+             deleteItem= {this.deleteItem}/>
              <form onSubmit={this.handleSubmit}>
              <input 
              onChange={this.handleChange}
-             value={this.state.inputValue}
+             value={this.state.currentItem.inputValue}
              />  
              <button type="submit">add</button>
             </form>
